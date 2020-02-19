72883e550dff6bd6c26fa336f754676c1fd1c888
loonj <loonj@163.com>
2020-02-19 12:02:50 +0800

    新增password, student.java

---
+++ cross/src/main/java/cn/zhanx/cross/domain/Student.java
@@ -0,0 +1,32 @@
+package cn.zhanx.cross.domain;
+
+public class Student {
+
+    private String name;
+    private Integer age;
+    private Integer sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+}
