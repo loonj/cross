d04235f75e4ea8ab891b3c448e9e8d26901b289d
loonj <loonj@163.com>
2020-02-19 11:59:18 +0800

    增加password

--- cross/src/main/java/cn/zhanx/cross/domain/User.java
+++ cross/src/main/java/cn/zhanx/cross/domain/User.java
@@ -2,6 +2,7 @@ package cn.zhanx.cross.domain;
 
 public class User {
     private String username;
+    private String password;
 
     public String getUsername() {
         return username;
@@ -9,5 +10,13 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
