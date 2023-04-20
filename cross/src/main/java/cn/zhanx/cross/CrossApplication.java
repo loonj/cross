d963be8f03519cee98ff004383d43d9e542b861a
loonj <loonj@163.com>
2023-04-20 14:22:32 +0800

    更新数据库配置，主文件打印日志

--- cross/src/main/java/cn/zhanx/cross/CrossApplication.java
+++ cross/src/main/java/cn/zhanx/cross/CrossApplication.java
@@ -9,6 +9,8 @@ public class CrossApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(CrossApplication.class, args);
+        System.out.println("dev1.0000");
+        System.out.println("dev1.0000");
     }
 
 }
